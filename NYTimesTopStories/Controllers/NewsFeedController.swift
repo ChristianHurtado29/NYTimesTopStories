7032c2baae9a5d3200b3e98339e9e109dc42169d
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-02-06 11:25:05 -0500

    reviewing constraints

--- NYTimesTopStories/Controllers/NewsFeedController.swift
+++ NYTimesTopStories/Controllers/NewsFeedController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class NewsFeedController: UIViewController {
     
+    private let newsFeed = NewsFeedView()
+    
+    override func loadView() {
+        view = newsFeed
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
     
 }
